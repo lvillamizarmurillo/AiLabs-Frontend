0ed3b07183c7ba5d33eb4a032b147e4a118a51fc
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2024-05-30 07:36:51 -0500

    feat: :tada: Repo y estructura para el frontend finalizada
    Se hace la estructura basica a seguir para el repo de frontend

---
+++ src/components/Global.jsx
@@ -0,0 +1,13 @@
+function Navegadora() {
+    return (
+        <>
+            <h1>Nav</h1>
+        </>
+    )
+}
+
+
+
+export {
+    Navegadora
+};
---
+++ src/components/views/Home.jsx
@@ -0,0 +1,14 @@
+import {Navegadora} from '../Global.jsx';
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+
+  return (
+    <>
+      <Navegadora />
+      <h1>Home</h1>
+    </>
+  );
+}
+
+export default Home;
