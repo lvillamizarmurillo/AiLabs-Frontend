71666683ed7db317e27b5f8cc06d7f5c614d1d70
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2024-06-08 12:12:44 -0500

    feat: :sparkles: Mi equipo maquetado y diseñado al 100%, solo falta agregar funcionalidad

--- src/components/views/Team.jsx
+++ src/components/views/Team.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import '../../assets/css/team.css';
 
-
-
 const Team = () => {
-
   return (
     <>
       <div className="containerPincipal">
@@ -12,154 +9,229 @@ const Team = () => {
           <h2>lvillamizar</h2>
         </div>
         <div className="primer-nivel">
-          <div className="user1">
-            <div className="container-lineas-niveles">
-
-            </div>
-            <div className="segundo-nivel">
-              <div className="user2">
-                <div className="container-lineas-niveles">
-
-                </div>
-                <div className="tercer-nivel">
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
+          <div className="linea-arriba1">
+            <div className="user1">
+              <div className="username">
+                <p>userA</p>
+              </div>
+              <div className="container-lineas-niveles1"></div>
+              <div className="segundo-nivel">
+                <div className="linea-arriba2">
+                  <div className="user2">
+                    <div className="username">
+                      <p>userB</p>
                     </div>
-                    <div className="cuarto-nivel">
-                      <div className="user4">
-                        <div className="container-lineas-niveles">
-
-                        </div>
-                        <div className="quinto-nivel">
-                          <div className="user5">
-                            <div className="container-lineas-niveles">
-                              
+                    <div className="container-lineas-niveles2-final"></div>
+                  </div>
+                  <div className="user2">
+                    <div className="username">
+                      <p>userC</p>
+                    </div>
+                    <div className="container-lineas-niveles2"></div>
+                    <div className="tercer-nivel">
+                      <div className="linea-arriba3">
+                        <div className="user3">
+                          <div className="user-final2">.</div>
+                          <div className="username">
+                            <p>userD</p>
+                          </div>
+                          <div className="container-lineas-niveles3"></div>
+                          <div className="cuarto-nivel">
+                            <div className="linea-arriba4">
+                              <div className="user4">
+                                <div className="username">
+                                  <p>userE</p>
+                                </div>
+                                <div className="container-lineas-niveles4-final"></div>
+                              </div>
+                              <div className="user4">
+                                <div className="user-final4">.</div>
+                                <div className="username">
+                                  <p>userF</p>
+                                </div>
+                                <div className="container-lineas-niveles4"></div>
+                                <div className="quinto-nivel">
+                                  <div className="linea-arriba5">
+                                    <div className="user5">
+                                      <div className="user-final4">.</div>
+                                      <div className="username">
+                                        <p>userG</p>
+                                      </div>
+                                      <div className="container-lineas-niveles5-final"></div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
+                  <div className="user2">
+                    <div className="user-final2">.</div>
+                    <div className="username">
+                      <p>userH</p>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div className="user2">
-                <div className="container-lineas-niveles">
-
-                </div>
-                <div className="tercer-nivel">
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="primer-nivel">
-          <div className="user1">
-            <div className="container-lineas-niveles">
-
-            </div>
-            <div className="segundo-nivel">
-              <div className="user2">
-                <div className="container-lineas-niveles">
-
-                </div>
-                <div className="tercer-nivel">
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
-                    </div>
-                    <div className="cuarto-nivel">
-                      <div className="user4">
-                        <div className="container-lineas-niveles">
-
+                    <div className="container-lineas-niveles2"></div>
+                    <div className="tercer-nivel">
+                      <div className="linea-arriba3">
+                        <div className="user3">
+                          <div className="username">
+                            <p>userI</p>
+                          </div>
+                          <div className="container-lineas-niveles3"></div>
+                          <div className="cuarto-nivel">
+                            <div className="linea-arriba4">
+                              <div className="user4">
+                                <div className="user-final4">.</div>
+                                <div className="username">
+                                  <p>userJ</p>
+                                </div>
+                                <div className="container-lineas-niveles4"></div>
+                                <div className="quinto-nivel">
+                                  <div className="linea-arriba5">
+                                    <div className="user5">
+                                      <div className="username">
+                                        <p>userK</p>
+                                      </div>
+                                      <div className="container-lineas-niveles5-final"></div>
+                                    </div>
+                                    <div className="user5">
+                                      <div className="user-final4">.</div>
+                                      <div className="username">
+                                        <p>userM</p>
+                                      </div>
+                                      <div className="container-lineas-niveles5-final"></div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div className="quinto-nivel">
-                          <div className="user5">
-                            <div className="container-lineas-niveles">
-                              
+                        <div className="user3">
+                          <div className="user-final3">.</div>
+                          <div className="username">
+                            <p>userN</p>
+                          </div>
+                          <div className="container-lineas-niveles3-final"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="user1">
+              <div className="user-final4">.</div>
+              <div className="username">
+                <p>userO</p>
+              </div>
+              <div className="container-lineas-niveles1"></div>
+              <div className="segundo-nivel">
+                <div className="linea-arriba2">
+                  <div className="user2">
+                    <div className="username">
+                      <p>userP</p>
+                    </div>
+                    <div className="container-lineas-niveles2-final"></div>
+                  </div>
+                  <div className="user2">
+                    <div className="username">
+                      <p>userQ</p>
+                    </div>
+                    <div className="container-lineas-niveles2"></div>
+                    <div className="tercer-nivel">
+                      <div className="linea-arriba3">
+                        <div className="user3">
+                          <div className="user-final2">.</div>
+                          <div className="username">
+                            <p>userR</p>
+                          </div>
+                          <div className="container-lineas-niveles3"></div>
+                          <div className="cuarto-nivel">
+                            <div className="linea-arriba4">
+                              <div className="user4">
+                                <div className="username">
+                                  <p>userS</p>
+                                </div>
+                                <div className="container-lineas-niveles4-final"></div>
+                              </div>
+                              <div className="user4">
+                                <div className="user-final4">.</div>
+                                <div className="username">
+                                  <p>userT</p>
+                                </div>
+                                <div className="container-lineas-niveles4"></div>
+                                <div className="quinto-nivel">
+                                  <div className="linea-arriba5">
+                                    <div className="user5">
+                                      <div className="user-final4">.</div>
+                                      <div className="username">
+                                        <p>userV</p>
+                                      </div>
+                                      <div className="container-lineas-niveles5-final"></div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
+                  <div className="user2">
+                    <div className="user-final2">.</div>
+                    <div className="username">
+                      <p>userW</p>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div className="user2">
-                <div className="container-lineas-niveles">
-
-                </div>
-                <div className="tercer-nivel">
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="primer-nivel">
-          <div className="user1">
-            <div className="container-lineas-niveles">
-
-            </div>
-            <div className="segundo-nivel">
-              <div className="user2">
-                <div className="container-lineas-niveles">
-
-                </div>
-                <div className="tercer-nivel">
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
-                    </div>
-                    <div className="cuarto-nivel">
-                      <div className="user4">
-                        <div className="container-lineas-niveles">
-
-                        </div>
-                        <div className="quinto-nivel">
-                          <div className="user5">
-                            <div className="container-lineas-niveles">
-                              
+                    <div className="container-lineas-niveles2"></div>
+                    <div className="tercer-nivel">
+                      <div className="linea-arriba3">
+                        <div className="user3">
+                          <div className="username">
+                            <p>userX</p>
+                          </div>
+                          <div className="container-lineas-niveles3"></div>
+                          <div className="cuarto-nivel">
+                            <div className="linea-arriba4">
+                              <div className="user4">
+                                <div className="user-final4">.</div>
+                                <div className="username">
+                                  <p>userY</p>
+                                </div>
+                                <div className="container-lineas-niveles4"></div>
+                                <div className="quinto-nivel">
+                                  <div className="linea-arriba5">
+                                    <div className="user5">
+                                      <div className="username">
+                                        <p>userZ</p>
+                                      </div>
+                                      <div className="container-lineas-niveles5-final"></div>
+                                    </div>
+                                    <div className="user5">
+                                      <div className="user-final4">.</div>
+                                      <div className="username">
+                                        <p>userAA</p>
+                                      </div>
+                                      <div className="container-lineas-niveles5-final"></div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
+                        <div className="user3">
+                          <div className="user-final3">.</div>
+                          <div className="username">
+                            <p>userBB</p>
+                          </div>
+                          <div className="container-lineas-niveles3-final"></div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="user2">
-                <div className="container-lineas-niveles">
-
-                </div>
-                <div className="tercer-nivel">
-                  <div className="user3">
-                    <div className="container-lineas-niveles">
-
                     </div>
                   </div>
                 </div>
